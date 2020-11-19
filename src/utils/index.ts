0b07ac03b9e415f27e222ce4c241942f2970f34d
YasirSherzad <ys.sherzad@gmail.com>
2020-11-19 21:03:40 +0430

    add scale util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,10 +5,12 @@ import ThemeDark from './theme/dark';
 // dimesions
 import * as Dimensions from './dimensions';
 import * as Shape from './shape';
+import * as Scale from './scale';
 
 export {
     ThemeLight,
     ThemeDark,
     Dimensions,
-    Shape
+    Shape,
+    Scale
 };
---
+++ src/utils/scale.ts
@@ -0,0 +1,15 @@
+import { SCREEN_HEIGHT, SCREEN_WIDTH } from './dimensions';
+
+//Guideline sizes are based on standard ~5" screen mobile device
+const guidelineBaseWidth = 350;
+const guidelineBaseHeight = 680;
+
+const scale = (size: number) => (SCREEN_WIDTH / guidelineBaseWidth) * size;
+const verticalScale = (size: number) => (SCREEN_HEIGHT / guidelineBaseHeight) * size;
+const moderateScale = (size: number, factor = 0.1) => size + (scale(size) - size) * factor;
+
+export {
+    scale,
+    verticalScale,
+    moderateScale
+};
