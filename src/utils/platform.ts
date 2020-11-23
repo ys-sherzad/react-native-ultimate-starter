a7bcdd89a88330e8eec6d4d19bd468e0bb4096b9
YasirSherzad <ys.sherzad@gmail.com>
2020-11-24 00:05:00 +0430

    add platform util

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -3,11 +3,13 @@ import { Platform } from 'react-native';
 const isIOS = Platform.OS === 'ios';
 const isAndroid = Platform.OS === 'android';
 const isTVOS = Platform.isTV;
+const isAndroidVersion21AndAbove = Platform.Version >= 21;
 // const isAndroidRTL = I18nManager.isRTL && isAndroid;
 // const isDeviceTablet = isTablet();
 
 export {
     isIOS,
     isAndroid,
-    isTVOS
+    isTVOS,
+    isAndroidVersion21AndAbove
 };
