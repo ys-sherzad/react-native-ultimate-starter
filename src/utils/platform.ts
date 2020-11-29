9f3b1847c3ad90775d68a0e681b29f753eca5494
YasirSherzad <ys.sherzad@gmail.com>
2020-11-29 14:00:59 +0430

    clean platform file

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -4,11 +4,7 @@ import { Platform } from 'react-native';
 
 const isIOS = Platform.OS === 'ios';
 const isAndroid = Platform.OS === 'android';
-const isTVOS = Platform.isTV;
 const isAndroidVersion21AndAbove = Platform.Version >= 21;
-// const isAndroidRTL = I18nManager.isRTL && isAndroid;
-// const isDeviceTablet = isTablet();
-
 
 /**
  * Add other platform utils
@@ -17,6 +13,5 @@ const isAndroidVersion21AndAbove = Platform.Version >= 21;
 export {
     isIOS,
     isAndroid,
-    isTVOS,
     isAndroidVersion21AndAbove
 };
