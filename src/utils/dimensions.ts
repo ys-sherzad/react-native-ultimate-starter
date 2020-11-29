5d9f979ea9ccc169ee16ee178fb42e9054a09c63
YasirSherzad <ys.sherzad@gmail.com>
2020-11-29 14:00:46 +0430

    update dimensions file

--- src/utils/dimensions.ts
+++ src/utils/dimensions.ts
@@ -1,15 +1,13 @@
 'use strict';
 
 import { Dimensions } from 'react-native';
-// @ts-ignore
-import ExtraDimensions from 'react-native-extra-dimensions-android';
 import { isIOS } from './platform';
 
 /**
  * device's height and width dimensions
  */
 const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = isIOS ? Dimensions.get('window').height : ExtraDimensions.getRealWindowHeight();
+const SCREEN_HEIGHT = isIOS ? Dimensions.get('window').height : require('react-native-extra-dimensions-android').get('REAL_WINDOW_HEIGHT');
 
 
 /**
